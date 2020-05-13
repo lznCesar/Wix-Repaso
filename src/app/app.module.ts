047afdd4d1f13f193a61138cdc2ef0f270c4bd65
lznCesar <celori54@gmail.com>
2020-05-13 12:42:05 -0500

    primer commit por pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/Home/home/home.component';
+import { CarosuelComponent } from './components/Home/carosuel/carosuel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CarosuelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/Home/carosuel/carosuel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carosuel',
+  templateUrl: './carosuel.component.html',
+  styleUrls: ['./carosuel.component.css']
+})
+export class CarosuelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
